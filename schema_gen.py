5e118be550e470cfed4c153f4d93b5acc4fe41ef
Vinnie Magro <v@vinn.ie>
2020-01-24 21:24:49 -0500

    work on schema generation from xml

---
+++ schema_gen.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import graphene
+from lxml import etree
+
+
+def gen_field(elem: etree.Element):
+    print(elem)
+
+    # if there are no children and no attributes, return the text node as a
+    # simple scalar type
+    if not len(elem) and not elem.attrib:
+        print("No children and no attributes")
+        # it might be a string, int, float or bool and will be disambiguated
+        # later by the resolver
+        return graphene.Scalar()
+    if not len(elem) and elem.attrib:
+        # ObjectType does a lot of introspection at class creation time, so do
+        # all our temporary work in this class
+        # class elemtype_tmp(object):
+        #     __name__ = elem.tag
+
+        # for key, _ in elem.attrib.items():
+        #     setattr(elemtype_tmp, key, graphene.Scalar())
+
+        # print(elemtype_tmp)
+        print(f"adding fields {list(elem.attrib.keys())}")
+
+        class elemtype(graphene.ObjectType):
+            class Meta:
+                name = elem.tag
+                fields = {
+                    # TODO: try to find a type now?
+                    key: graphene.Field(graphene.String)
+                    for key in elem.attrib.keys()
+                }
+            hardcoded = graphene.String()
+
+        print(f"meta = {elemtype._meta}")
+        print(f"meta fields = {elemtype._meta.fields}")
+
+        return elemtype
---
+++ schema_gen_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import graphene
+from lxml import etree
+
+from schema_gen import gen_field
+
+
+def test_gen_string_text():
+    e = etree.fromstring(r"<hello>textchild</hello>")
+    field = gen_field(e)
+    assert field == graphene.Scalar()
+
+
+def test_gen_attr_no_child():
+    e = etree.fromstring(r'<hello answer="42"/>')
+    field = gen_field(e)
+    # TODO: figure out how to properly introspect this
+
+    class Query(graphene.ObjectType):
+        root = graphene.Field(field)
+
+    schema = graphene.Schema(query=Query)
+    print(schema.execute("{ root { answer } }"))
+    print(schema)
