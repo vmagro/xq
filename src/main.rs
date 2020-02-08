0a7c8160f55ebe52e2e9dc1152e39033c15d582f
Vinnie Magro <v@vinn.ie>
2020-02-08 10:47:32 -0500

    panic if more than one definition

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,8 @@ use graphql_parser::parse_query;
 fn main() {
     println!("Hello, world!");
     let ast = parse_query("query { field1, field2 }").unwrap();
-    println!("{:?}", ast);
+    if ast.definitions.len() != 1 {
+        panic!("Must have exactly 1 definition");
+    }
+    println!("{:?}", ast.definitions[0]);
 }
