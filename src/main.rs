f2a211703c82a0ef6471db44f5f4a15d8c03a17e
Vinnie Magro <v@vinn.ie>
2020-02-08 16:27:37 -0500

    parse json from stdin

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ use graphql_parser::parse_query;
 use graphql_parser::query::{Definition, OperationDefinition};
 
 fn main() {
-    // let doc = roxmltree::Document::parse(std::include_str!("example.xml")).unwrap();
-    // let xml_root = doc.root_element();
-    let json_root = &serde_json::from_str(std::include_str!("example.json")).unwrap();
+    let json_root = &serde_json::from_reader(std::io::stdin()).unwrap();
 
     // let ast = parse_query("query { books { author } }").unwrap();
     let ast = parse_query("query { top_level, books { author }}").unwrap();
