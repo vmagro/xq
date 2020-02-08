de443d10dee924a7ad8da332c7b95dafe297c339
Vinnie Magro <v@vinn.ie>
2020-02-08 12:20:29 -0500

    basic test setup

--- tests/json.rs
+++ tests/json.rs
@@ -1,4 +1,31 @@
+use graphql_parser::query::Query;
+use graphql_parser::query::{Definition, OperationDefinition};
+use serde_json::json;
+
+use xq::eval;
+
+fn parse_query(q: &str) -> Query {
+    let ast = graphql_parser::parse_query(q).unwrap();
+
+    let query_root = &ast.definitions[0];
+
+    match query_root {
+        Definition::Operation(OperationDefinition::Query(q)) => {
+            return q.clone();
+        }
+        _ => {
+            panic!("Unsupported root: {:?}", query_root);
+        }
+    }
+}
+
 #[test]
-fn it_adds_two() {
-    assert_eq!(4, 2 + 2);
+fn eval_missing_key() {
+    let src = json!({});
+    let query = parse_query("query { no_such_key }");
+    let res = eval(&query, src);
+    assert!(res.is_object());
+    let obj = res.as_object().unwrap();
+    assert!(obj.contains_key("no_such_key"));
+    assert_eq!(obj.get("no_such_key").unwrap(), &json!(null));
 }
