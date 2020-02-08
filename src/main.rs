f2ad43f7bd078e19070aa9f29e6998b14fc57943
Vinnie Magro <v@vinn.ie>
2020-02-08 11:03:54 -0500

    some query parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 use graphql_parser::parse_query;
+use graphql_parser::query::{Definition, OperationDefinition, Selection};
 
 fn main() {
     println!("Hello, world!");
-    let ast = parse_query("query { field1, field2 }").unwrap();
+    let ast = parse_query("query { root1, root2 { nested1 }, aliased: root3 }").unwrap();
     if ast.definitions.len() != 1 {
         panic!("Must have exactly 1 definition");
     }
-    println!("{:?}", ast.definitions[0]);
+    let root = &ast.definitions[0];
+    match root {
+        Definition::Operation(OperationDefinition::Query(q)) => {
+            for sel in &q.selection_set.items {
+                match sel {
+                    Selection::Field(f) => {
+                        println!("Selecting field {}", f.name);
+                    }
+                    _ => {
+                        panic!("Unsupported root: {:?}", root);
+                    }
+                }
+            }
+        }
+        _ => {
+            panic!("Unsupported root: {:?}", root);
+        }
+    }
 }
